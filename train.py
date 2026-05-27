3a8b0537c7f4dbb6f32dbabf39233fe26a648376
mark-175 <152170112+mark-175@users.noreply.github.com>
2026-05-27 16:05:12 +0200

    Code untill step 1.3

--- train.py
+++ train.py
@@ -6,3 +6,23 @@ import joblib
 
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 print(df.head())
+
+df.dropna(inplace=True)
+print(df.info())
+print("====================")
+
+df['gender'] = df['gender'].map({'Male': 1, 'Female': 0, 'Other': 0})
+print(df['gender'].value_counts())
+print("====================")
+
+print(df['smoking_status'].value_counts())
+df['smoking_status'] = df['smoking_status'].map(
+    {'never smoked': 0, 'formerly smoked': 1, 'smokes': 2, 'Unknown': -1})
+print(df['smoking_status'].value_counts())
+print("====================")
+
+FEATURES = ["age", "hypertension", "heart_disease",
+            "avg_glucose_level", "bmi", "gender", "smoking_status"]
+
+X = df[FEATURES]
+y = df["stroke"]
