964138627e5643798418a010286d99866a82c2b5
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2026-05-27 15:58:44 +0200

    app base

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
-from flask import Flask, jsonify, request
-
+from flask import Flask, jsonify, request, render_template
+import joblib
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.get("/")
 def home():
-	return jsonify({"message": "Home endpoint is running."}), 200
+    return render_template("index.html", prediction=None, patient=None)
 
 
-@app.post("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
-	payload = request.get_json(silent=True) or {}
-	return (
-		jsonify(
-			{
-				"message": "Predict endpoint is ready.",
-				"input": payload,
-				"prediction": None,
-			}
-		),
-		200,
-	)
+    payload = request.get_json(silent=True) or {}
+    return (
+        jsonify(
+            {
+                "message": "Predict endpoint is ready.",
+                "input": payload,
+                "prediction": None,
+            }
+        ),
+        200,
+    )
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
